10a9bdfd06f5a74c7e4f94e1da14b0e216cc6331
Stackbit Code Editor <projects@stackbit.com>
2021-10-29 03:30:50 +0000

    LogoSection.js: changed

--- src/components/LogoSection.js
+++ src/components/LogoSection.js
@@ -15,7 +15,6 @@ const LogoSection = (props) => {
             limit = 5;
         }
 
-        if (limit)
         api_url + 
         const url = "https://api.thedogapi.com/v1/breeds?limit=5";
         const fetchData = async () => {
